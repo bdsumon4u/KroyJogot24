4bd867f6477f0c2ded229ff21c68c2734a2c9289
Sumon Ahmed <bdsumon4u@gmail.com>
2023-06-20 08:47:27 +0600

    Deleted UnNecessaryFiles

--- resources/views/layouts/light/css.blade.php
+++ resources/views/layouts/light/css.blade.php
@@ -4,14 +4,6 @@
 <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900&amp;display=swap" rel="stylesheet">
 <!-- Font Awesome-->
 <link rel="stylesheet" type="text/css" href="{{asset('assets/css/fontawesome.css')}}">
-<!-- ico-font-->
-<link rel="stylesheet" type="text/css" href="{{asset('assets/css/icofont.css')}}">
-<!-- Themify icon-->
-<link rel="stylesheet" type="text/css" href="{{asset('assets/css/themify.css')}}">
-<!-- Flag icon-->
-<link rel="stylesheet" type="text/css" href="{{asset('assets/css/flag-icon.css')}}">
-<!-- Feather icon-->
-<link rel="stylesheet" type="text/css" href="{{asset('assets/css/feather-icon.css')}}">
 <!-- Plugins css start-->
 @stack('css')
 <!-- Plugins css Ends-->
--- resources/views/layouts/light/js.blade.php
+++ resources/views/layouts/light/js.blade.php
@@ -3,9 +3,6 @@
 <!-- Bootstrap js-->
 <script src="{{asset('assets/js/bootstrap/popper.min.js')}}"></script>
 <script src="{{asset('assets/js/bootstrap/bootstrap.js')}}"></script>
-<!-- feather icon js-->
-<script src="{{asset('assets/js/icons/feather-icon/feather.min.js')}}"></script>
-<script src="{{asset('assets/js/icons/feather-icon/feather-icon.js')}}"></script>
 <!-- Sidebar jquery-->
 <script src="{{asset('assets/js/sidebar-menu.js')}}"></script>
 <!-- Plugins JS start-->
